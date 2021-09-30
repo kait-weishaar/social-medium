f12256edbe95db73897d3b0f42f1f194e3a26a52
Kait <kweishaar@hawk.iit.edu>
2021-09-29 20:06:22 -0500

    don't need a reaction model

--- models/thought.js
+++ models/thought.js
@@ -59,7 +59,7 @@ const ThoughtSchema = new Schema(
     }
 );
 
-//reaction virtual count
+//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
 const Thought = model("Thought", ThoughtSchema);
 
--- models/user.js
+++ models/user.js
@@ -40,7 +40,7 @@ const UserSchema = new Schema(
     }
 );
 
-//Use virtual for friendcount
+//Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 
 //Create model
