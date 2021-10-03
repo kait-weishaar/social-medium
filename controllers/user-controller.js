7a402dc8486d296ffcac89e7529ab06017c8eebc
Kait <kweishaar@hawk.iit.edu>
2021-10-02 21:39:30 -0500

    progress on user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,65 @@
 //Need User and will be nicer to import populate as destructured so don't have to write user.populate
 
 const {User} = require("../models");
-const {populate} = require("../models/User");
+// const {populate} = require("../models/User");
+
+
+const userController = {
+    // get all
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: "thoughts",
+            select: "-__v"
+        })
+        .select("-__v")
+        .sort({_id: -1})
+        .then((dbUserData)=> res.json(dbUserData))
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+    },
+
+    //get user by id
+    getUserbyId({params}, res) {
+        User.findOne({_id: params.id})
+        .populate({
+            path: "thoughts",
+            select: "-__v",
+        
+        })
+        .select("-__v")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this ID" });
+                return;
+              }
+              res.json(dbUserData);
+        });
+    },
+
+    //crud
+    createUser({body}, res) {
+        User.create(body)
+        ,then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(400).json(err));
+    },
+
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate(
+            {_id:params.id},
+            body,
+            {new: true})
+
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this ID" });
+                    return;
+                  }
+                  res.json(dbUserData);
+                })
+                .catch((err) => res.status(400).json(err));
+        
+    },
+    
+}
